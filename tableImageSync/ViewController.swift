82c86402bd2d12ce1afd8dd8d286efe201bb114f
Anton <sektor.shik@outlook.com>
2021-07-06 20:51:52 +0300

    Initial Commit

---
+++ tableImageSync/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableImageSync
+//
+//  Created by Shylak Anton on 6.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
